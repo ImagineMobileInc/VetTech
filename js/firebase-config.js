25eb2f56115cd099d825228ec4a6b76dc3f05fb5
ImagineMobileInc <phil@customblogs.net>
2025-02-28 09:43:12 -0500

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,14 @@
+// js/firebase-config.js
+export function initializeFirebase() {
+    const firebaseConfig = {
+        apiKey: "AIzaSyCglcioqoTDldyrRUARq9qXAHK8LF6pO-s",
+        authDomain: "vetcalc-f447a.firebaseapp.com",
+        projectId: "vetcalc-f447a",
+        storageBucket: "vetcalc-f447a.firebasestorage.app",
+        messagingSenderId: "552275376699",
+        appId: "1:552275376699:web:ac1a7f7453e23d395fba1e"
+    };
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+}
